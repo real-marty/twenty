5fd68f38ff9a98a053c5b53911d0204d23180cca
martmull <martmull@hotmail.fr>
2025-06-05 14:39:02 +0200

    WIP: add add step in workflow arrows

--- packages/twenty-front/src/modules/workflow/workflow-diagram/components/WorkflowDiagramDefaultEdge.tsx
+++ packages/twenty-front/src/modules/workflow/workflow-diagram/components/WorkflowDiagramDefaultEdge.tsx
@@ -1,6 +1,7 @@
 import { useTheme } from '@emotion/react';
 import { BaseEdge, EdgeProps, getStraightPath } from '@xyflow/react';
 import { CREATE_STEP_NODE_WIDTH } from '@/workflow/workflow-diagram/constants/CreateStepNodeWidth';
+import { VERTICAL_DISTANCE_BETWEEN_TWO_NODES } from '@/workflow/workflow-diagram/constants/VerticalDistanceBetweenTwoNodes';
 
 type WorkflowDiagramDefaultEdgeProps = EdgeProps;
 
@@ -20,11 +21,27 @@ export const WorkflowDiagramDefaultEdge = ({
   });
 
   return (
-    <BaseEdge
-      markerStart={markerStart}
-      markerEnd={markerEnd}
-      path={edgePath}
-      style={{ stroke: theme.border.color.strong }}
-    />
+    <>
+      <BaseEdge
+        markerStart={markerStart}
+        markerEnd={markerEnd}
+        path={edgePath}
+        style={{ stroke: theme.border.color.strong }}
+      />
+      <foreignObject
+        width={40}
+        height={30}
+        x={50}
+        y={(VERTICAL_DISTANCE_BETWEEN_TWO_NODES + 30) / 2}
+        requiredExtensions="http://www.w3.org/1999/xhtml"
+      >
+        <button
+          onClick={() => {}}
+          style={{ width: '100%', height: '100%', fontSize: 10 }}
+        >
+          +
+        </button>
+      </foreignObject>
+    </>
   );
 };
